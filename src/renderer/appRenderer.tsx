34b37e654f2539909d3b6efb689f1a041c52f3ed
pepsiamir <noorani.lord@gmail.com>
2022-09-29 12:18:50 +0330

    working version

--- src/renderer/appRenderer.tsx
+++ src/renderer/appRenderer.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import WindowFrame from '@misc/window/components/WindowFrame';
 import Application from '@components/Application';
+import SuperTokens, {
+  SuperTokensWrapper,
+  getSuperTokensRoutesForReactRouterDom,
+} from 'supertokens-auth-react';
+import ThirdPartyEmailPasswordReact from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
+import Session from 'supertokens-auth-react/recipe/session';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import SessionExpiredPopup from '@misc/window/components/SessionExpiredPopup';
+import getCookieHandler from '@misc/window/components/cookieHandler';
+import getWindowHandler from '@misc/window/components/windowHandler';
 
 // Say something
 console.log('[ERWT] : Renderer execution started');
